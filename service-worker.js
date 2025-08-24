e604f4cfaf99b499e884869c792d9e90349801c3
victorgpd <victorguilherme2407@outlook.com>
2025-08-24 16:08:38 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,8 +2,8 @@ self.addEventListener("push", (event) => {
   const data = event.data ? event.data.text() : "Nova notificação!";
   const options = {
     body: data,
-    // icon: "/icon.png", // opcional
-    // badge: "/badge.png", // opcional
+    icon: "./isologo.png", // opcional
+    icon: "./isologo.png", // opcional
   };
   event.waitUntil(self.registration.showNotification("Claybot", options));
 });
